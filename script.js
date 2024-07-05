36945107c83c5f0132e29dfa4e49f5c2f57c2263
ebagabee <gabriel.ebagabe2001@gmail.com>
2024-07-04 22:47:53 -0300

    Site finalizado

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ function scrollProjects(direction) {
 
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 darkModeToggle.addEventListener('change', () => {
-    document.body.classList.toggle('dark-mode'); // Aplica uma classe no body para estilização dark mode
+    document.body.classList.toggle('dark-mode');
 });
 
 updateLanguage();
